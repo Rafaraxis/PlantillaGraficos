86cdc4f88833c845ac3c04bd5385b7f551639715
alumno <alumno@D3A03>
2018-01-22 09:30:39 -0700

    Add project files.

---
+++ Graficos/Graficos.cpp
@@ -0,0 +1,93 @@
+// Graficos.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#define GLEW_STATIC
+#include "GL\glew.h"
+#include "GLFW\glfw3.h"
+
+#include <iostream>
+using namespace std;
+
+void actualizar()
+{
+
+}
+
+void dibujar()
+{
+
+}
+
+int main()
+{
+	//Declaración de Ventana
+	GLFWwindow *window;
+	
+	//Propiedades de la ventana
+	GLfloat ancho = 1024;
+	GLfloat alto = 768;
+
+	//Inicialización de GLFW
+	if (!glfwInit())
+	{
+		//Si no se inicia bien termina la ejecución
+		exit(EXIT_FAILURE);
+	}
+
+	//Inicialización de la ventana
+	window = glfwCreateWindow(ancho, alto, "Gráficos", NULL, NULL);
+
+	//Verificación de creación de ventana
+	if (!window)
+	{
+		//Cerrar todos los procesos de GLFW
+		glfwTerminate();
+		//Termina ejecución
+		exit(EXIT_FAILURE);
+	}
+
+
+	//Establecer "windows" como contexto
+	glfwMakeContextCurrent(window);
+
+	//Se trae las funciones de OpenGL Moderno
+	glewExperimental = true;
+
+	//Inicializar GLEW
+	GLenum glewError = glewInit();
+
+	//Verificar si GLEW se inicializo bien
+	if (glewError != GLEW_OK)
+	{
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	//Ciclo de Dibujo
+	while (!glfwWindowShouldClose(window))
+	{
+		//Establecer el area de render (viewport)
+		glViewport(0, 0, ancho, alto);
+		//Establecer el color con el que se limpia
+		glClearColor(1, 0.8, 0, 1);
+		//Limpiar pantalla
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		//Rutina de Dibujo
+		actualizar();
+		dibujar();
+
+		//Intercambio de Buffers
+		glfwSwapBuffers(window);
+		//Buscar nueva señal de entrada
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+    return 0;
+}
+
